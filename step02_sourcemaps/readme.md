4a973e36191940c3de8cd9dce0c13d5616a096b3
Zia Khan <ziaukhan@hotmail.com>
2016-06-10 13:55:33 +0500

    added step 2

---
+++ step02_sourcemaps/readme.md
@@ -0,0 +1,9 @@
+Read Generating source maps Section of:
+
+http://www.pro-react.com/materials/appendixA/
+
+To build:
+
+npm start
+
+open public/index.html in browser
---
+++ step02_sourcemaps/webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  devtool: 'eval-source-map',
+  entry:  __dirname + "/app/main.js",
+  output: {
+    path: __dirname + "/public",
+    filename: "bundle.js"
+  }
+}
